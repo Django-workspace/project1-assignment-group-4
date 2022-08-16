b703bacb1c45e00c9997053d0c3dd1742d9c4317
mashami <mashamipaccy04@gmail.com>
2022-08-16 09:17:14 +0200

    accounts sign Up

--- E_Tunanire/E_Tunanire/settings.py
+++ E_Tunanire/E_Tunanire/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -144,8 +145,8 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mashamipaccy04@gmail.com'
-EMAIL_HOST_PASSWORD = 'vwfi clod dkns jioc' 
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
@@ -155,3 +156,5 @@ DEFAULT_FROM_EMAIL = 'default from email'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend' 
+
+
